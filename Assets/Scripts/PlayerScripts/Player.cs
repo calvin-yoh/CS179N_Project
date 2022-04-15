b7e5014b8c3d21843df26dbe9c59fa38e81c81ca
myong003 <matthew.yong0801@gmail.com>
2022-04-15 14:18:17 -0700

    made some changes

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -6,21 +6,27 @@ public class Player : MonoBehaviour
 {
     public int numStartingCards = 5;
 
-    private bool canPlaceStudent = true;
-    private bool canPlaceFaculty = true;
-    private List<Card> hand;
+    // private bool canPlaceStudent = true;
+    // private bool canPlaceFaculty = true;
+    public HandLayout hand;
     public List<Card> openDeck;
     private Stack<Card> deck;
+    public GameObject display;
 
     public void StartTurn(){
-        canPlaceFaculty = true;
-        canPlaceStudent = true;
+        // canPlaceFaculty = true;
+        // canPlaceStudent = true;
         DrawCard();
     }
 
     public void DrawCard(){
-        hand.Add(deck.Pop());
-        Debug.Log(this.name + " has " + hand.Count + " cards");
+        // hand.Add(deck.Pop());
+        Card newCard = deck.Pop();
+        // CardDisplay newDisplay = new CardDisplay(newCard);
+        var gameob = Instantiate(display, gameObject.transform);
+        gameob.GetComponent<CardDisplay>().card = newCard;
+        hand.AddCard(gameob.GetComponent<CardDisplay>());
+        // Debug.Log(this.name + " has " + hand.Count + " cards");
     }
 
     public void PlaceCard(Card card){
@@ -33,7 +39,7 @@ public class Player : MonoBehaviour
 
     void Start(){
         deck = new Stack<Card>();
-        hand = new List<Card>(20);
+        // hand = new List<Card>(20);
         for (int i=0; i < openDeck.Count; i++){
             deck.Push(openDeck[i]);
         }
