d1ba198b729e6c4a033e2e793a9ebde8eb391446
Bryan Guevara <bryan19052000@gmail.com>
2022-04-27 02:45:12 -0700

    updates LoadCards.cs script

--- Assets/Card Selection/LoadCards.cs
+++ Assets/Card Selection/LoadCards.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class LoadCards : MonoBehaviour
 {
 
-    // Thesse lsts are not needed right now. But they will be used later to reference cards.
     List<BuildingCard> BuildingCards = new List<BuildingCard>();
     List<StudentCard> StudentCards = new List<StudentCard>();
     List<FacultyCard> FacultyCards = new List<FacultyCard>();
@@ -13,6 +12,7 @@ public class LoadCards : MonoBehaviour
 
     public GameObject BuildingCardDisplayPrefab;
     public GameObject StudentCardDisplayPrefab;
+    public GameObject FacultyCardDisplayPrefab;
 
     void displayStudentCard(StudentCard card){
         var go = Instantiate(StudentCardDisplayPrefab, transform.position, transform.rotation);
@@ -22,7 +22,15 @@ public class LoadCards : MonoBehaviour
         go.transform.SetParent(this.transform);
     }
 
-    void dislayBuildingCard(BuildingCard card){
+    void displayFacultyCard(FacultyCard card){
+        var go = Instantiate(FacultyCardDisplayPrefab, transform.position, transform.rotation);
+        go.GetComponent<FacultyCardDisplay>().card = card;
+        go.GetComponent<FacultyCardDisplay>().DisplayInformation();
+
+        go.transform.SetParent(this.transform);
+    }
+
+    void displayBuildingCard(BuildingCard card){
         var go = Instantiate(BuildingCardDisplayPrefab, transform.position, transform.rotation);
         go.GetComponent<BuildingCardDisplay>().card = card;
         go.GetComponent<BuildingCardDisplay>().DisplayInformation();
@@ -32,45 +40,56 @@ public class LoadCards : MonoBehaviour
     
     // Start is called before the first frame update
     void Start()
-    {
-        Object[] R_BuildingCards = Resources.LoadAll("Cards/BuildingCards", typeof(BuildingCard));
+    {   
+        // Loading faculty cards (NO FACULTY CARDS YET);
+
+        // Loading student cards
         Object[] R_ArtStudentCards = Resources.LoadAll("Cards/StudentCards/Art Students", typeof(StudentCard));
         Object[] R_AthleticStudentCards = Resources.LoadAll("Cards/StudentCards/Athletic Studnets", typeof(StudentCard));
         Object[] R_EngineeringStudentCards = Resources.LoadAll("Cards/StudentCards/Engineering Students", typeof(StudentCard));
         Object[] R_StaffStudentCards = Resources.LoadAll("Cards/StudentCards/Staff Students", typeof(StudentCard));
+        
+        // Loading Buiding cards.
+        Object[] R_BuildingCards = Resources.LoadAll("Cards/BuildingCards", typeof(BuildingCard));
 
 
         // Adding all the student cards to the StudentCards list.
         foreach(Object x in R_ArtStudentCards){
             StudentCard card = (StudentCard) x;
             StudentCards.Add(card);
-            displayStudentCard(card);
         }
 
         foreach(Object x in R_AthleticStudentCards){
             StudentCard card = (StudentCard) x;
             StudentCards.Add(card);
-            displayStudentCard(card);
         }
 
         foreach(Object x in R_EngineeringStudentCards){
             StudentCard card = (StudentCard) x;
             StudentCards.Add(card);
-            displayStudentCard(card);
         }
 
         foreach(Object x in R_StaffStudentCards){
             StudentCard card = (StudentCard) x;
             StudentCards.Add(card);
-            displayStudentCard(card);
         }
 
         // Adding all building cards
         foreach(Object x in R_BuildingCards){
             BuildingCard card = (BuildingCard) x;
             BuildingCards.Add(card);
-            dislayBuildingCard(card);
         }
+
+
+
+        // Displaying all cards.
+        StudentCards.ForEach(card => {
+            displayStudentCard(card);
+        });
+
+        BuildingCards.ForEach(card => {
+            displayBuildingCard(card);
+        });
 
 
 
