8ae65c7bffa44d9c1712b0b6b3aacfe324d6e3ac
ThePurplePhoenix <45699039+ThePurplePhoenix@users.noreply.github.com>
2022-04-27 23:07:44 -0700

    Menu updates

---
+++ Assets/Scripts/UI_Scripts/Exit.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit: MonoBehaviour {  
+    public void exitgame() {  
+        Debug.Log("exitgame");  
+        Application.Quit();  
+    }  
+}
