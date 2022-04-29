4dd1b0823615bfc620fa1d277d09720d794e968d
Bryan Guevara <bryan19052000@gmail.com>
2022-04-29 14:47:56 -0700

    Adds selected cards column

---
+++ Assets/clickable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickable : MonoBehaviour
+{
+    void OnMouseDown(){
+        Debug.Log("Woring");
+    }
+}
