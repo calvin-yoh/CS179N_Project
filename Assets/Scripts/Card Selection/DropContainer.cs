23b27bcd1f6b457b5dc48c8a286c9f745322dbbe
Bryan Guevara <bryan19052000@gmail.com>
2022-05-13 14:16:08 -0700

    updates text on deck header

--- Assets/Scripts/Card Selection/DropContainer.cs
+++ Assets/Scripts/Card Selection/DropContainer.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System;
+using UnityEngine.UI;
 
 
 public class DropContainer : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData){
-        if (eventData.pointerDrag != null){
-            var copy = Instantiate(eventData.pointerDrag, transform.position, transform.rotation);
-            copy.GetComponent<RectTransform>().SetParent(this.transform);
-        }
+        string[] accepted = {"UIStudentCard(Clone)", "UIBuildingCard(Clone)", "UIFacutlyCard(Clone)"};
+        if (Array.IndexOf(accepted, eventData.pointerDrag.name) == -1){ return; }
+        if (transform.childCount >= 20){ return; }
 
+        Debug.Log(eventData.pointerDrag);
+        var copy = Instantiate(eventData.pointerDrag, transform.position, transform.rotation);
+        copy.GetComponent<RectTransform>().SetParent(this.transform);
+        copy.GetComponent<DragAndDrop>().selected = true;
 
         RectTransform rec = GetComponent<RectTransform>();
 
@@ -26,5 +31,8 @@ public class DropContainer : MonoBehaviour, IDropHandler
         if (new_height > rec.sizeDelta.y){
             rec.sizeDelta = new Vector2(rec.sizeDelta.x, new_height);
         }
+
+
+        transform.parent.gameObject.GetComponentInChildren<Text>().text =  "Deck: " + transform.childCount.ToString() + " / 20";
     }
 }
