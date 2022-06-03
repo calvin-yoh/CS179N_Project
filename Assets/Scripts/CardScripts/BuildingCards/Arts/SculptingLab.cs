7ef6434b9e720bc67636d09c209594336fc7c120
Calvin Yoh <flashfire340@gmail.com>
2022-06-02 20:17:14 -0700

    Update

---
+++ Assets/Scripts/CardScripts/BuildingCards/Arts/SculptingLab.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SculptingLab : CardEffect
+{
+    protected override void Start()
+    {
+        targetType = Card.Type.Student;
+        targetTeam = TargetTeam.Friendly;
+        numTargets = 0;
+    }
+
+    private void OnEnable()
+    {
+        Player currPlayer = GameManager.Instance.GetCurrentPlayer();
+        EventsManager em = currPlayer.GetEventsManager();
+        em.OnCardPlayedFromHand += CardPlayedFromHandPassive;
+    }
+
+    private void OnDisable()
+    {
+        Player currPlayer = GameManager.Instance.GetCurrentPlayer();
+        EventsManager em = currPlayer.GetEventsManager();
+        em.OnCardPlayedFromHand -= CardPlayedFromHandPassive;
+    }
+
+    //The opponent's student cards have -1 duration when played.
+    public override int PerformEffect(GameData data)
+    {
+        return 0;
+    }
+
+    public override void CardPlayedFromHandPassive(CardDisplay card)
+    {
+        StudentCardDisplay scd = card.GetComponent<StudentCardDisplay>();
+        if (scd != null)
+        {
+            if (scd.playerNumber == GameManager.Instance.GetCurrentPlayer().number)
+            {
+                Debug.Log("ConcertHall building increased dur");
+                scd.ChangeDurationBy(1);
+            }
+        }
+    }
+}
