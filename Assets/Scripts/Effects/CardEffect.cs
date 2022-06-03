88c1066d255aca40b23761606c7d8a1d23efad3f
Calvin Yoh <flashfire340@gmail.com>
2022-06-02 23:33:51 -0700

    Update

--- Assets/Scripts/Effects/CardEffect.cs
+++ Assets/Scripts/Effects/CardEffect.cs
@@ -24,7 +24,7 @@ public abstract class CardEffect : MonoBehaviour
     /// <returns></returns>
     public abstract int PerformEffect(GameData data);
 
-    public virtual void CardPlayedFromHandPassive(CardDisplay card) { return; }
+    public virtual void CardPassive(CardDisplay card) { return; }
 
     //Flip a coin. 0 for Tails. 1 for Head
     public int FlipCoin(int luckModifier)
